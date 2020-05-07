2657f1f91568f95cb1edfedb002a274f01376363
bauticanavese <bauti-canavese@hotmail.com>
2020-05-07 17:10:41 -0300

    add view and handle add video

--- app/src/main/java/com/example/chotuvemobileapp/MainActivity.kt
+++ app/src/main/java/com/example/chotuvemobileapp/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.chotuvemobileapp
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.provider.OpenableColumns
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity() : AppCompatActivity() {
+    var uri = null as Uri?
+    lateinit var mStorageRef : StorageReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +26,45 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        mStorageRef = FirebaseStorage.getInstance().reference;
     }
 
+    fun onClickAddVideo(view: View) {
+        val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+            type = "video/*"
+        }
+
+        startActivityForResult(Intent.createChooser(intent, "Select video"), 0)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK) {
+            uri = data!!.data
+        }
+    }
+
+    private fun getFileName(uri: Uri) : String {
+        var result = null as String?
+        if (uri.scheme.equals("content")) {
+            val cursor = contentResolver.query(uri, null, null, null, null)
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                cursor!!.close();
+            }
+        }
+
+        if (result == null) {
+            result = uri.getPath();
+            val cut = result!!.lastIndexOf('/')
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result
+    }
 }
