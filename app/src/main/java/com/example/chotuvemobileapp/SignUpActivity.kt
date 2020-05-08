412948b85165e9c0dbbabc9e6d6dbb5678d314fa
Federico Alvarez Alonso <falonso@arredo.com.ar>
2020-05-08 18:24:39 -0300

    Check password > 6 caracteres

--- app/src/main/java/com/example/chotuvemobileapp/SignUpActivity.kt
+++ app/src/main/java/com/example/chotuvemobileapp/SignUpActivity.kt
@@ -110,8 +110,12 @@ class SignUpActivity : AppCompatActivity() {
         RegPassFirst.error = null
         RegDate.error = null
 
+        if (RegPwFirstText.text.toString().length < 6){
+            RegPassFirst.error = getString(R.string.invalid_password)
+            valid = false
+        }
         if (RegPwFirstText.text.toString() != RegPwSecondText.text.toString()){
-            RegPassFirst.error = getString(R.string.password_mismatch)
+            RegPassSecond.error = getString(R.string.password_mismatch)
             valid = false
         }
         if (!android.util.Patterns.EMAIL_ADDRESS.matcher(RegEmailText.text.toString()).matches() ||
