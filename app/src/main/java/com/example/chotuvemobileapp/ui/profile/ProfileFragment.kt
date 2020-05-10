f112f741fcf870ae28cc3441f93c721f0d1de0ae
Federico Alvarez Alonso <falonso@arredo.com.ar>
2020-05-10 15:11:41 -0300

    Tweaks a las vistas

---
+++ app/src/main/java/com/example/chotuvemobileapp/ui/profile/ProfileFragment.kt
@@ -0,0 +1,24 @@
+package com.example.chotuvemobileapp.ui.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.chotuvemobileapp.R
+
+class ProfileFragment : Fragment() {
+
+    private lateinit var profileViewModel: ProfileViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        profileViewModel =
+            ViewModelProvider(this).get(profileViewModel::class.java)
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+}
