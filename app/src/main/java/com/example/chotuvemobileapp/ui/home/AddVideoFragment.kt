62dc389ee8c8cc312cd78d9ec5a25176d8159adb
Federico Alvarez Alonso <falonso@arredo.com.ar>
2020-06-28 20:09:14 -0300

    Agregado pedido de permisos más ubicación TODO:    -Chequear por que la ubicación es siempre null

--- app/src/main/java/com/example/chotuvemobileapp/ui/home/AddVideoFragment.kt
+++ app/src/main/java/com/example/chotuvemobileapp/ui/home/AddVideoFragment.kt
@@ -1,9 +1,11 @@
 package com.example.chotuvemobileapp.ui.home
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.provider.OpenableColumns
@@ -13,6 +15,7 @@ import android.view.ViewGroup
 import android.view.WindowManager
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
@@ -22,7 +25,9 @@ import com.example.chotuvemobileapp.data.videos.Video
 import com.example.chotuvemobileapp.data.videos.VideoDataSource
 import com.example.chotuvemobileapp.helpers.PickRequest
 import com.example.chotuvemobileapp.helpers.Utilities.DATE_FORMAT_LONG
+import com.example.chotuvemobileapp.helpers.Utilities.REQUEST_LOCATION_PERMISSION
 import com.example.chotuvemobileapp.helpers.Utilities.getFileName
+import com.google.android.gms.location.LocationServices
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.fragment_add_video.*
@@ -34,12 +39,17 @@ class AddVideoFragment : Fragment() {
     private var uri = null as Uri?
     private var fileSize = null as String?
     private var fileName = null as String?
+    private var locationEnabled = false
     private val mStorageRef by lazy {
         FirebaseStorage.getInstance().reference
     }
     private val prefs by lazy {
         requireActivity().applicationContext.getSharedPreferences(getString(R.string.shared_preferences_file),
         Context.MODE_PRIVATE)
+    }
+
+    private val locationClient by lazy {
+        LocationServices.getFusedLocationProviderClient(requireActivity())
     }
 
     override fun onCreateView(
@@ -53,6 +63,9 @@ class AddVideoFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), REQUEST_LOCATION_PERMISSION)
+        }
         AddVideoToolbar.setNavigationOnClickListener{
             val home=  activity as HomeActivity
             home.openDrawer()
@@ -74,29 +87,17 @@ class AddVideoFragment : Fragment() {
                 storageReference.putFile(uri!!)
                     .addOnSuccessListener {   // Get a URL to the uploaded content
                         storageReference.downloadUrl.addOnSuccessListener { uri ->
-                            val videoToSend = Video(VideoTitleInputText.text.toString(),
-                                VideoDescriptionInputText.text.toString(),
-                                if (publicRadioButton.isChecked) "public" else "private",
-                                uri.toString(),
-                                nowDateTimeStr(),
-                                fileName!!,
-                                fileSize!!
-                            )
-                            VideoDataSource.addVideo(videoToSend, prefs){
-                                when(it){
-                                    "Success" ->{
-                                        UploadVideoProgressBar.visibility = View.GONE
-                                        requireActivity().window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
-                                        AddVideoScreen.alpha = 1F
-                                        Toast.makeText(context, getString(R.string.video_uploaded_message), Toast.LENGTH_LONG).show()
-                                        findNavController().navigate(R.id.nav_home)
-                                    }
-                                    else -> {
-                                        fail()
-                                        Toast.makeText(context, getString(R.string.internal_error), Toast.LENGTH_LONG).show()
-                                    }
+                            if (locationEnabled) {
+                                locationClient.lastLocation.addOnSuccessListener { loc ->
+                                    val lat = loc?.latitude
+                                    val long = loc?.longitude
+                                    sendVideo(uri, lat, long)
+                                }.addOnFailureListener {
+                                    fail()
+                                    Toast.makeText(context, getString(R.string.internal_error), Toast.LENGTH_LONG).show()
                                 }
                             }
+                            else sendVideo(uri, null, null)
                         }.addOnFailureListener{ Exception ->
                             Toast.makeText(context, Exception.message, Toast.LENGTH_LONG).show()
                             fail()
@@ -116,6 +117,40 @@ class AddVideoFragment : Fragment() {
                 type = "video/*"
             }
             startActivityForResult(Intent.createChooser(intent, "Select Video"), PickRequest.Video.value)
+        }
+    }
+
+    private fun sendVideo(uri: Uri, lat: Double?, long: Double?) {
+        val videoToSend = Video(
+            VideoTitleInputText.text.toString(),
+            VideoDescriptionInputText.text.toString(),
+            if (publicRadioButton.isChecked) "public" else "private",
+            uri.toString(),
+            nowDateTimeStr(),
+            fileName!!,
+            fileSize!!,
+            lat,
+            long
+        )
+        VideoDataSource.addVideo(videoToSend, prefs) {
+            when (it) {
+                "Success" -> {
+                    UploadVideoProgressBar.visibility = View.GONE
+                    requireActivity().window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+                    AddVideoScreen.alpha = 1F
+                    Toast.makeText(
+                        context,
+                        getString(R.string.video_uploaded_message),
+                        Toast.LENGTH_LONG
+                    ).show()
+                    findNavController().navigate(R.id.nav_home)
+                }
+                else -> {
+                    fail()
+                    Toast.makeText(context, getString(R.string.internal_error), Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
         }
     }
 
@@ -165,5 +200,15 @@ class AddVideoFragment : Fragment() {
         val current = LocalDateTime.now()
         val formatter = DateTimeFormatter.ofPattern(DATE_FORMAT_LONG)
         return current.format(formatter)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_LOCATION_PERMISSION){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) locationEnabled = true
+        }
     }
 }
